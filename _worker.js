f66d242685057ab6ff6666581a4c733e821458a5
xiaoxxiao <113592485+lemongac@users.noreply.github.com>
2024-02-27 20:50:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ["[2001:67c:2960:6464::6812:cae8]", "[2001:67c:2960:6464::6812:cbe8]"];
+const proxyIPs = ["[2001:67c:2960:6464::6812:cae8]", "[2001:67c:2960:6464::6812:cbe8]", "[2001:67c:2960:6464::6812:d82]", "[2001:67c:2960:6464::6812:c82]"];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
